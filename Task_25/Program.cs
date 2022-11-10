39ecafe928ef758c20175c5c84b93b52c10831af
Katy <bev08021982@gmail.com>
2022-11-10 04:03:00 +0400

    Решила 25 задачу

--- Task_25/Program.cs
+++ Task_25/Program.cs
@@ -2,4 +2,40 @@
 //возводит число A в натуральную степень B.
 //3, 5 -> 243 (3⁵)
 //2, 4 -> 16
-Console.WriteLine("Введите .");
+
+//Функция расчета степени и в положительную и в отрецательную степень.
+decimal Exponentiation(int num, int exp)
+{
+    decimal sum = num;
+    decimal res = sum;
+    if (exp > 0)
+    {
+        for (int i = 2; i <= exp; i++)
+        {
+            sum *= num;
+        }
+        return sum;
+    }
+    else
+    {
+        for (int i = exp; i <= -2; i++)
+        {
+            sum *= num;
+            res = 1 / sum;
+        }
+        return res;
+    }
+}
+// Получение данных от пользователя
+Console.Write("Введите основание степени:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number <= 0)
+{
+    Console.WriteLine("Вы ввели не натуральное число. Попробуйте ещё.");
+    return;
+}
+Console.Write("Введите показатель степени:");
+int exponent = Convert.ToInt32(Console.ReadLine());
+
+//Вывод результата
+Console.Write($"{number}^{exponent} = {Exponentiation(number, exponent)}");
